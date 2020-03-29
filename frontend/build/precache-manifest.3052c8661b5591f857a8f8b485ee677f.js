1007b7b2feefe1fe50361885ed77207d8e6786c4
Gabriel Nascimento <gabriel.nascimento3110@outlook.com>
2020-03-29 16:57:55 -0300

    update files frontend

---
+++ frontend/build/precache-manifest.3052c8661b5591f857a8f8b485ee677f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0bab9f49defcc17d089a99d046e506c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cd0e9c9ce9f8e69a3a47",
+    "url": "/static/css/main.64733756.chunk.css"
+  },
+  {
+    "revision": "19d7d3b4baacab3db35d",
+    "url": "/static/js/2.2b93c1ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2b93c1ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd0e9c9ce9f8e69a3a47",
+    "url": "/static/js/main.df00be97.chunk.js"
+  },
+  {
+    "revision": "055e94f6602794288cc3",
+    "url": "/static/js/runtime-main.1f1f19a3.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "62e865e13ce4dd36023d6f8d6c53bb73",
+    "url": "/static/media/logo.62e865e1.svg"
+  }
+]);
